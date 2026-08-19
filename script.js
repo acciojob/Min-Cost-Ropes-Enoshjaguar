b40266aa4c0c04082c4183d715dcc3aaa0816462
Enoshjaguar <dasarienosh013@gmail.com>
2026-08-19 20:54:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+let len = arr.length
+
+	let res=[]
+	let costmin = 0
+	res =arr
+	while(len>1){
+		res=res.sort((a,b)=>a-b)
+		let cost = res[0]+res[1]
+		mincost=costmin+cost
+		let newarr;
+		newarr.push(...res.slice(2),cost)
+		res = newarr
+
+		len=len-1
+	}
+	return costmin
   
 }
 
